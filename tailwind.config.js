f87d5f4dfbb6827f1bb1d1482e7ae822126d9501
akhilpamwani <83491855+akhilpamwani@users.noreply.github.com>
2022-07-29 14:22:50 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
+/** @type {import('tailwindcss').Config} */ 
 module.exports = {
-   
-  darkMode: "className",
-  content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './pages/**/*.js", "./components/**/*.js',
-      './components/**/*.{js,ts,jsx,tsx}',
-      './node_modules/tw-elements/dist/js/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
       extend: {},
-  },
-  plugins: [
-   
-  ], }
+    },
+    plugins: [],
+  }
